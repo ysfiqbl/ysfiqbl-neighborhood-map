a76f67245444e854d76a94a0326daf7fe17326e0
Yusuf Iqbal <ysf.iqbl@gmail.com>
2015-04-22 07:59:50 +0530

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+    	cssmin : {
+            css:{
+            	files: {
+                	'dist/css/main.min.css': ['css/main.css']
+                }
+            }
+        },
+        uglify : {
+        	js: {
+	            files: {
+	                'dist/js/scripts/app.min.js' : [ 'js/scripts/app.js' ]
+	            }
+	        }
+	    },
+	    htmlmin: {
+		   dist: {
+		      options: {
+		         removeComments: true,
+		         collapseWhitespace: true
+		      },
+		      files: [{
+		         expand: true,
+		         cwd: '.',
+		         src: ['index.html', 'orders/*.html'],
+		         dest: 'dist/'
+		      }]
+		   }
+		},
+	    watch: {
+			files: ['css/*', 'js/*', '.'],
+			tasks: ['concat', 'cssmin', 'uglify']
+	   	}
+    });
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', [ 'cssmin:css', 'uglify:js', 'htmlmin' ])
+};
