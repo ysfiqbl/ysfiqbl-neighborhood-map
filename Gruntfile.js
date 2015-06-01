d42ac1830548d2e65817733cee38856527ed8fe1
Yusuf Iqbal <ysf.iqbl@gmail.com>
2015-06-01 22:17:08 +0530

    Fixed grunt file and added jshint to grunt. Now grunt watch works as intended.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,33 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
+		jshint: {
+			options: {
+				reporter: require('jshint-stylish')
+			},
+			build: ['Gruntfile.js', 'js/scripts/*.js']
+		},
+		uglify : {
+			options: {
+				banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/'
+			},
+			build: {
+		        files: {
+		            'dist/js/scripts/app.min.js' : [ 'js/scripts/app.min.js' ]
+		        }
+		    }
+		},
     	cssmin : {
-            css:{
+    		options: {
+        		banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/'
+        	},
+            build:{
             	files: {
                 	'dist/css/offline.min.css': ['css/offline.min.css'],
                 	'dist/css/main.min.css': ['css/main.min.css']
                 }
             }
         },
-        uglify : {
-        	js: {
-	            files: {
-	                'dist/js/scripts/app.min.js' : [ 'js/scripts/app.min.js' ]
-	            }
-	        }
-	    },
 	    htmlmin: {
 		   dist: {
 		      options: {
@@ -26,19 +37,30 @@ module.exports = function(grunt) {
 		      files: [{
 		         expand: true,
 		         cwd: '.',
-		         src: ['index.html', 'orders/*.html'],
+		         src: ['index.html'],
 		         dest: 'dist/'
 		      }]
 		   }
 		},
 	    watch: {
-			files: ['css/*', 'js/*', '.'],
-			tasks: ['concat', 'cssmin', 'uglify']
+	    	stylesheets: {
+	    		files: 'css/*.css',
+	    		tasks: 'cssmin'
+	    	},
+	    	scripts: {
+	    		files: 'js/scripts/*.js',
+	    		tasks: ['jshint', 'uglify']
+	    	},
+	    	html: {
+	    		files: 'index.html',
+	    		tasks: 'htmlmin'
+	    	}
 	   	}
     });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('default', [ 'cssmin:css', 'uglify:js', 'htmlmin' ])
+    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'htmlmin' ]);
 };
